94d54648d6ebc483f438359a0a03c6d948a453e5
GabrielReyesOrtiz <greyes17@ucol.mx>
2023-05-06 12:46:19 -0600

    encuesta

--- src/app.js
+++ src/app.js
@@ -1,38 +1,61 @@
-const express = require('express');
-const { engine } = require('express-handlebars');
-const myconnection = require('express-myconnection');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const tasksRoutes = require('./routes/tasks');
+const express = require('express');  // Importa la librería Express
+const { engine } = require('express-handlebars');  // Importa el motor de plantillas Handlebars
+const myConnection = require('express-myconnection');  // Importa la librería express-myconnection
+const bodyParser = require('body-parser');  // Importa la librería body-parser para procesar datos de formularios
+const mysql = require('mysql');  // Importa el controlador de MySQL
+const session = require('express-session');  // Importa la librería express-session para el manejo de sesiones
+const loginRoutes = require('./routes/login')  // Importa el módulo de rutas de login
 
-const app = express();
-app.set('port', 80);
+const app = express();  // Crea una instancia de la aplicación Express
+app.set('port', 3000);  // Establece el puerto en el que se ejecutará la aplicación
 
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({  // Agrega middleware para procesar datos de formularios
   extended: true
 }));
 app.use(bodyParser.json());
 
-app.set('views', __dirname + '/views');
-app.engine('.hbs', engine({
-  extname: '.hbs',
-}));
-app.set('view engine', 'hbs');
-
-app.use(myconnection(mysql, {
-  host: 'localhost',
+app.use(myConnection(mysql, {  // Agrega middleware para establecer la conexión con la base de datos
+  host: '127.0.0.1',
   user: 'root',
   password: '1234',
   port: 3306,
-  database: 'crudnodejsmysql'
+  database: 'registrolaboral2023'
 }, 'single'));
 
-app.listen(app.get('port'), () => {
+app.get('/verificar-conexion', (req, res) => {  // Ruta para verificar la conexión con la base de datos
+  req.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error de conexión a la base de datos:', err);
+      res.send('Error de conexión a la base de datos');
+    } else {
+      console.log('Conexión a la base de datos establecida');
+      res.send('Conexión a la base de datos establecida');
+    }
+  });
+});
+
+app.use(session({  // Agrega middleware para el manejo de sesiones
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.set('views', __dirname + '/views');  // Establece la carpeta donde se encuentran las vistas
+app.engine('.hbs', engine({  // Configura el motor de plantillas Handlebars
+  extname: '.hbs',
+}));
+app.set('view engine', 'hbs');  // Establece el motor de plantillas predeterminado
+
+app.listen(app.get('port'), () => {  // Inicia la aplicación en el puerto especificado
   console.log('Listening on port ', app.get('port'));
 });
 
-app.use('/', tasksRoutes);
+app.use('/', loginRoutes);  // Agrega las rutas de login
 
-app.get('/', (req, res) => {
-  res.render('home');
+app.get('/', (req, res) => {  // Ruta para la página principal
+  if(req.session.loggedin != true) {  // Verifica si el usuario ha iniciado sesión
+    res.render('home', { name: req.session.name });  // Si no ha iniciado sesión, muestra la página de inicio
+} else {
+    res.redirect('/login')  // Si ha iniciado sesión, redirige a la página de login
+}
 });
---
+++ src/controllers/LoginController.js
@@ -0,0 +1,92 @@
+
+//En este archivo estan todas las funciones utilizadas en el programa.
+
+//Función encargada de renderizar la hbs de index/login
+function login(req, res) {
+
+        res.render('login/index');
+    } 
+
+//Función encargada de realizar el login.
+function auth(req, res) {
+    const data = req.body;
+  
+    if (req.session.loggedin) {
+      res.redirect('/');
+      return;
+    } 
+
+    //Aqui se recuperan los datos de la base de datos a traves del nombre de usuario
+    //escrito en el input de la pagina login.
+    req.getConnection((err, conn) => {
+      conn.query('SELECT * FROM usuarios WHERE nombre = ?', [data.nombre], (err, userdata) => {
+        //En caos de que se encuentre algo, se entra en el if
+        if (userdata.length > 0) {
+          const element = userdata[0];
+          //Se comparan la contraseña guardada en la base de datos con la contraseña introducida en el input de la pagina
+          if (data.clave === element.clave) {
+           //req.session.loggedin = true;
+            req.session.name = element.nombre;
+            res.redirect('/');
+          } else {
+            res.render('login/index', { error: 'La contraseña es incorrecta' });
+          }
+        } else {
+          res.render('login/register', { error: 'El nombre de usuario no existe' });
+        }
+      })
+    })
+  } 
+
+  //Función encargada de renderizar la hbs de register
+  function register(req, res) {
+    if (req.session.loggedin) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login/register');
+  }
+
+  //Función que realiza el registro de usuarios.
+function storeUser(req, res) {
+    const data = req.body;
+    
+    req.getConnection((err, conn) => {
+        //Se busca si existe un nombre de usuario registrado en la base de datos
+        //que sea igual al introducido en el input de registrar usuario.
+        conn.query('SELECT * FROM registros WHERE nombreLegal = ?', [data.nombreLegal], (err, userdata) => {
+            
+          //Se revisa si existe respuesta por parte de la base de datos
+            if(userdata.length > 0) {
+                res.render('login/register', { error: 'El nombre de usuario ya existe'});
+            } else {
+              //Se realiza el registro del usuario.
+                req.getConnection((err, conn) => {
+                conn.query('INSERT INTO registros SET ?', [data], (err, rows) => {
+
+                //req.session.loggedin = true;
+                req.session.name = "Gracias " + data.nombrePrefe;
+
+            res.redirect('/');
+        });
+    });
+            }
+        });
+    });
+    
+}
+
+//Funcion que se encarga de realizar el logout o cerrar la sesión
+function logout(req, res) {
+  
+    res.redirect('/register'); 
+}
+
+//Aqui se exportan todas las funciones realizadas para ser utilizadas por login.js
+module.exports = {
+    login,
+    register,
+    storeUser,
+    auth,
+   logout,
+}
